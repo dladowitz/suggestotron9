03b13b75116fb7070fd6e53ad220e390241c705e
David Ladowitz <david@ladowitz.com>
2013-12-28 16:47:04 -0800

    finished building the things

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Suggestotron9::Application.routes.draw do
+
+  root 'topics#index'
+
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +48,7 @@ Suggestotron9::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
